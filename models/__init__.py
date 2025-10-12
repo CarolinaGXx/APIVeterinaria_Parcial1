fe2c7fd828ffef968874728b20a976e908d5d0c2
bufonen <santigon8787@gmail.com>
2025-10-12 14:38:48 -0500

    feat: corrigiendo init de models

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@ from .mascotas import Mascota, MascotaCreate, MascotaUpdate, TipoMascota
 from .citas import Cita, CitaCreate, CitaUpdate, EstadoCita
 from .vacunas import Vacuna, VacunaCreate, VacunaUpdate, TipoVacuna
 from .facturas import Factura, FacturaCreate, FacturaUpdate, EstadoFactura, TipoServicio
+from .recetas import Receta, RecetaCreate, RecetaSummary, RecetaUpdate
+from .usuarios import Usuario, UsuarioCreate, Role, UsuarioUpdateResponse, UsuarioUpdateRequest
 
 __all__ = [
     # Mascotas
@@ -11,5 +13,9 @@ __all__ = [
     # Vacunas
     "Vacuna", "VacunaCreate", "VacunaUpdate", "TipoVacuna",
     # Facturas
-    "Factura", "FacturaCreate", "FacturaUpdate", "EstadoFactura", "TipoServicio"
+    "Factura", "FacturaCreate", "FacturaUpdate", "EstadoFactura", "TipoServicio",
+    # Usuarios
+    "Usuario", "UsuarioCreate", "Role", "UsuarioUpdateResponse", "UsuarioUpdateRequest",
+    # Recetas
+    "Receta", "RecetaCreate", "RecetaSummary", "RecetaUpdate"
 ]
